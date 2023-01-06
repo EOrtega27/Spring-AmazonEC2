4ed0176b8ddb41825ef5099b924032d6864c87c0
EOrtega27 <ortegachaveznsm@gmail.com>
2023-01-05 19:03:23 -0500

    AWS Properties

--- src/main/java/com/eddy/springAws/repositories/ContactoRepository.java
+++ src/main/java/com/eddy/springAws/repositories/ContactoRepository.java
@@ -3,8 +3,10 @@ package com.eddy.springAws.repositories;
 import com.eddy.springAws.models.Contacto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "contactos", collectionResourceRel = "contactos")
 public interface ContactoRepository extends JpaRepository<Contacto, Long> {
+    List<Contacto> findAllByOrderByName();
 }
